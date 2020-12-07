c8f6bce5316b9dd75e313da4eb339d8a9f02b59b
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-12-08 00:37:03 +0530

    files added with transition

---
+++ CASINO_Game_Slot/casino.js
@@ -0,0 +1,19 @@
+let value1 = document.getElementById('value1')
+let value1 = document.getElementById('value2')
+let value1 = document.getElementById('value3')
+
+let values = [
+    '🤍' ,'💜' ,'💛' ,'💖' ,'💚' ,'🖤' ,'🧡'
+]
+
+function getRandomValue() {
+    return values[Math.floor(Math.random() * 7)]
+}
+
+setInterval(() => {
+
+    value1.innerText = getRandomValue()
+    value2.innerText = getRandomValue()
+    value3.innerText = getRandomValue()
+
+},300)
